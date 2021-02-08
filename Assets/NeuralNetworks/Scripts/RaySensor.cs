578228dcd4f71d0c829b96b0e5bda75cd851f6c0
AxelAlavedra <axel.alavedra@gmail.com>
2021-02-08 19:08:37 +0100

    sensor input detection

--- Assets/NeuralNetworks/Scripts/RaySensor.cs
+++ Assets/NeuralNetworks/Scripts/RaySensor.cs
@@ -4,38 +4,75 @@ using UnityEngine;
 
 public class RaySensor : MonoBehaviour
 {
-    [Tooltip("Amount of rays that will be casted each side")]
-    public int rays = 2;
+    [SerializeField]
     [Tooltip("Amount of rays that will be casted")]
-    public int rayLength = 5;
-    [Tooltip("The amount of degrees for each side")]
-    public int degrees = 90;
+    private int rays = 5;
+    [SerializeField]
+    [Tooltip("Amount of rays that will be casted")]
+    private int rayLength = 5;
+    [SerializeField]
+    [Tooltip("The amount of degrees where rays will be casted in")]
+    private int degrees = 90;
+    [SerializeField]
+    [Tooltip("LayerMask used for hit detection")]
+    private LayerMask layerMask;
 
-    // Update is called once per frame
-    void Update()
+    float[] rayInputs;
+
+    private void Start()
     {
-
+        rayInputs = new float[rays + 1];
     }
+
+    public void AnalyzeSensors()
+    {
+        if (rays == 1)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.forward, out hit, rayLength, layerMask.value))
+                rayInputs[0] = (10 - hit.distance) / rayLength;
+            else
+                rayInputs[0] = 0;
+        }
+        else
+        {
+            float anglePerRay = degrees / (rays - 1);
+            float currentAngle = degrees * 0.5f;
+
+            for (int i = 0; i < rays; i++)
+            {
+                Vector3 direction = Quaternion.AngleAxis(currentAngle, Vector3.up) * transform.forward;
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, direction, out hit, rayLength, layerMask.value))
+                    rayInputs[i] = (10 - hit.distance) / rayLength;
+                else
+                    rayInputs[i] = 0;
+
+                currentAngle -= anglePerRay;
+            }
+        }
+    }
+
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawRay(transform.position, transform.forward * rayLength);
-
-        if (rays <= 0)
+        if (rays == 1)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, transform.forward * rayLength);
             return;
+        }
 
-        float anglePerRay = degrees / rays;
-        float currentAngle = -degrees;
+        float anglePerRay = degrees / (rays - 1);
+        float currentAngle = degrees * 0.5f;
 
-        for (int i = 0; i < rays * 2; i++)
+        for (int i = 0; i < rays; i++)
         {
             Vector3 direction = (Quaternion.AngleAxis(currentAngle, Vector3.up) * transform.forward) * rayLength;
+            Gizmos.color = Color.blue;
             Gizmos.DrawRay(transform.position, direction);
 
-            currentAngle += anglePerRay;
-
-            if (i == rays - 1) //Change side if half of rays are done
-                currentAngle = anglePerRay;
+            currentAngle -= anglePerRay;
         }
     }
 }
