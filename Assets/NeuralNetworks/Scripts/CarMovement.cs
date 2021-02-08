a27b77282610f004d7a972b7b7aeb923b9657e94
AxelAlavedra <axel.alavedra@gmail.com>
2021-02-08 18:19:59 +0100

    Raycast sensor configuration

---
+++ Assets/NeuralNetworks/Scripts/CarMovement.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CarMovement : MonoBehaviour
+{
+    [Header("Car Settings")]
+    [Tooltip("If the player will control the car or not")]
+    public bool playerControlled = false;
+    [Tooltip("Speed at which the car will accelerate")]
+    public float carSpeed = 50.0f;
+    [Tooltip("Speed at which the car will brake")]
+    public float brakeSpeed = 500.0f;
+    [Tooltip("Max steer angle the car can achieve")]
+    public float maxSteerAngle = 30.0f;
+    //[Tooltip("Max speed the car can achieve")]
+    //public float maxCarSpeed = 200.0f;
+
+    [Header("Car Info")]
+    [Tooltip("The parent gameObject of the wheels")]
+    public Transform wheelCollidersParentTransform;
+    [Tooltip("All the wheel transforms of the car")]
+    public List<Transform> wheelTransforms;
+    [SerializeField]
+    float steeringAngle, horizontalInput, brakeInput, verticalInput = .0f;
+
+    private List<WheelCollider> wheelColliders;
+    private enum WheelPosition { FrontRight, RearRight, FrontLeft, RearLeft };
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (playerControlled)
+            Camera.main.gameObject.SetActive(false);
+
+        wheelColliders = new List<WheelCollider>();
+
+        for(int i = 0; i < wheelCollidersParentTransform.childCount; i++)
+        {
+            wheelColliders.Add(wheelCollidersParentTransform.GetChild(i).GetComponent<WheelCollider>());
+        }
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses(); 
+    }
+
+    private void GetInput()
+    {
+        if(playerControlled)
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+            brakeInput = Input.GetAxis("Jump");
+        }
+    }
+
+    private void Accelerate()
+    {
+        wheelColliders[(int)WheelPosition.FrontLeft].motorTorque = verticalInput * carSpeed;
+        wheelColliders[(int)WheelPosition.FrontRight].motorTorque = verticalInput * carSpeed;
+
+        wheelColliders[(int)WheelPosition.FrontLeft].brakeTorque = brakeInput * brakeSpeed;
+        wheelColliders[(int)WheelPosition.FrontRight].brakeTorque = brakeInput * brakeSpeed;
+    }
+
+    private void Steer()
+    {
+        steeringAngle = maxSteerAngle * horizontalInput;
+
+        wheelColliders[(int)WheelPosition.FrontLeft].steerAngle = steeringAngle;
+        wheelColliders[(int)WheelPosition.FrontRight].steerAngle = steeringAngle;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        for (int i = 0; i < wheelTransforms.Count; i++)
+        {
+            Vector3 pos = wheelTransforms[i].position;
+            Quaternion quat = wheelTransforms[i].rotation;
+
+            wheelColliders[i].GetWorldPose(out pos, out quat);
+            wheelTransforms[i].position = pos;
+            wheelTransforms[i].rotation = quat;
+        }
+    }
+}
---
+++ Assets/NeuralNetworks/Scripts/RaySensor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaySensor : MonoBehaviour
+{
+    [Tooltip("Amount of rays that will be casted each side")]
+    public int rays = 2;
+    [Tooltip("Amount of rays that will be casted")]
+    public int rayLength = 5;
+    [Tooltip("The amount of degrees for each side")]
+    public int degrees = 90;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, transform.forward * rayLength);
+
+        if (rays <= 0)
+            return;
+
+        float anglePerRay = degrees / rays;
+        float currentAngle = -degrees;
+
+        for (int i = 0; i < rays * 2; i++)
+        {
+            Vector3 direction = (Quaternion.AngleAxis(currentAngle, Vector3.up) * transform.forward) * rayLength;
+            Gizmos.DrawRay(transform.position, direction);
+
+            currentAngle += anglePerRay;
+
+            if (i == rays - 1) //Change side if half of rays are done
+                currentAngle = anglePerRay;
+        }
+    }
+}
