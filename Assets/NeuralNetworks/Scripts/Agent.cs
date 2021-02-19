235bdb28e9401c0c44f4ccffed7a816117d2ffd8
AxelAlavedra <axel.alavedra@gmail.com>
2021-02-19 17:25:50 +0100

    Agent abstract class implemented

---
+++ Assets/NeuralNetworks/Scripts/Agent.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Axel.NeuralNetworks
+{
+    /// <summary>
+    /// Agent abstract class for Neural Network based behaviours.
+    /// </summary>
+    [RequireComponent(typeof(NeuralNetworkConfiguration))]
+    public class Agent : MonoBehaviour
+    {
+        [Header("Neural Network")]
+        [Tooltip("If the player will control the agent or not")]
+        public bool playerControlled = false;
+        [Tooltip("If the agent is training or not")]
+        public bool training = false;
+        [Tooltip("NN Graph for visualization of the Neural Network")]
+        public NNGraph graph;
+        [Tooltip("The neural network the agent is using")]
+        private NeuralNetwork brain;
+
+        //ToDo (Axel): Make NN initialization always be called, without having to call base Start on child class.
+        protected virtual void Start()
+        {
+            NeuralNetworkConfiguration NNConfig = GetComponent<NeuralNetworkConfiguration>();
+            brain = new NeuralNetwork(NNConfig);
+
+            // ToDo (Axel): improve UI initialization
+            if (graph != null)
+                graph.Initialize(brain);
+        }
+
+        /// <summary>
+        /// Function to add the inputs required for the Neural Network of this behaviour to work.
+        /// </summary>
+        /// <param name="input">Array to fill with inputs for the NN</param>
+        public virtual void AddObservationsInput(ref float[] input)
+        {
+            throw new System.NotImplementedException("AddInput function was not implemented on child class");
+        }
+
+        /// <summary>
+        /// Function gets called when an output for the agent behaviour has been obtained. This output can be NN or Player.
+        /// </summary>
+        /// <param name="output">The output arrray of the Agent</param>
+        public virtual void OnOutputReceived(float[] output)
+        {
+            throw new System.NotImplementedException("OnOutputReceived function was not implemented on child class");
+        }
+
+        /// <summary>
+        /// Function to add inputs of the Player to the Agent behaviour. OnOutputReceived gets called with this input.
+        /// </summary>
+        /// <param name="input"></param>
+        public virtual void AddPlayerInput(ref float[] input)
+        {
+            throw new System.NotImplementedException("PlayerInput function was not implemented on child class");
+        }
+
+        /// <summary>
+        /// Function that gets called when the cycle of training ends, so the Agent can get reset to original state and start training again.
+        /// </summary>
+        public virtual void OnReset()
+        {
+            throw new System.NotImplementedException("OnReset function was not implemented on child class");
+        }
+
+        public void Train()
+        {
+            // Backpropagation
+            // 5 sensors
+            // sensor 0 right
+            // sensor 1 front-right
+            // sensor 2 front
+            // sensor 3 front-left
+            // sensor 4 left
+            brain.BackPropagate(new float[] { 0, 0, 0, 0, 0 }, new float[] { 0, 1 });
+            brain.BackPropagate(new float[] { 1, 0, 0, 0, 0 }, new float[] { 0, 1 });
+            brain.BackPropagate(new float[] { 0, 1, 0, 0, 0 }, new float[] { -1.0f, 0.5f });
+            brain.BackPropagate(new float[] { 0, 0, 1, 0, 0 }, new float[] { 0, -1 });
+            brain.BackPropagate(new float[] { 0, 0, 0, 1, 0 }, new float[] { 1.0f, 0.5f });
+            brain.BackPropagate(new float[] { 0, 0, 0, 0, 1 }, new float[] { 0, 1 });
+            brain.BackPropagate(new float[] { 1, 1, 0, 0, 0 }, new float[] { -1, 0.5f });
+            brain.BackPropagate(new float[] { 1, 1, 1, 0, 0 }, new float[] { -1, -1 });
+            brain.BackPropagate(new float[] { 1, 1, 1, 1, 0 }, new float[] { 0, -1 });
+            brain.BackPropagate(new float[] { 0, 0, 0, 1, 1 }, new float[] { 1, 0.5f });
+            brain.BackPropagate(new float[] { 0, 0, 1, 1, 1 }, new float[] { 1, -1 });
+            brain.BackPropagate(new float[] { 0, 1, 1, 1, 1 }, new float[] { 0, -1 });
+            brain.BackPropagate(new float[] { 1, 0, 0, 0, 1 }, new float[] { 0, 1 });
+            brain.BackPropagate(new float[] { 1, 1, 0, 0, 1 }, new float[] { -1.0f, 0.5f });
+            brain.BackPropagate(new float[] { 1, 1, 1, 0, 1 }, new float[] { -1.0f, -1 });
+            brain.BackPropagate(new float[] { 1, 0, 0, 1, 1 }, new float[] { 1.0f, 0.5f });
+            brain.BackPropagate(new float[] { 1, 0, 1, 1, 1 }, new float[] { 1.0f, -1 });
+            brain.BackPropagate(new float[] { 1, 1, 0, 1, 1 }, new float[] { 0, 1 });
+            brain.BackPropagate(new float[] { 1, 1, 1, 1, 1 }, new float[] { 0, -1 });
+
+            //Todo Reinforced Learning Training
+        }
+
+        private void FixedUpdate()
+        {
+            float[] output = new float[brain.config.outputSize];
+
+            //If the agent is being player controlled, we use the player input as output for the agent behaviour.
+            if (playerControlled)
+                AddPlayerInput(ref output);
+            else
+            {
+                float[] input = new float[brain.config.inputSize];
+
+                //Gather the input from the agent (sensors and others)
+                AddObservationsInput(ref input);
+
+                //Feed the input to the Neural Network to obtain an output
+                output = brain.FeedForward(input);
+            }
+
+            //Send output to the agent behaviour
+            OnOutputReceived(output);
+
+            //If the neural network is training proceed to Train with selected method.
+            if (training)
+                Train();
+        }
+    }
+}
+
---
+++ Assets/NeuralNetworks/Scripts/CarAgent.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Axel.NeuralNetworks
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class CarAgent : Agent
+    {
+        [Header("Car Settings")]
+        [Tooltip("Speed at which the car will accelerate")]
+        public float carSpeed = 50.0f;
+        [Tooltip("Speed at which the car will brake")]
+        public float brakeSpeed = 500.0f;
+        [Tooltip("Max steer angle the car can achieve")]
+        public float maxSteerAngle = 30.0f;
+        //[Tooltip("Max speed the car can achieve")]
+        //public float maxCarSpeed = 200.0f;
+        Vector3 startPosition;
+        Quaternion startRotation;
+        public RaySensor raySensor;
+
+        [Header("Car Info")]
+        [Tooltip("The parent gameObject of the wheels")]
+        public Transform wheelCollidersParentTransform;
+        [Tooltip("All the wheel transforms of the car")]
+        public List<Transform> wheelTransforms;
+        [SerializeField]
+        float steeringAngle, horizontalInput, brakeInput, verticalInput = .0f;
+
+        private List<WheelCollider> wheelColliders;
+        private enum WheelPosition { FrontRight, RearRight, FrontLeft, RearLeft };
+
+
+
+        #region AgentFunctions
+        public override void OnOutputReceived(float[] output)
+        {
+            // output 0 horizontal input - steer
+            // output 1 vertical input - accelerate
+            horizontalInput = output[0];
+            verticalInput = output[1];
+
+            Steer();
+            Accelerate();
+            UpdateWheelPoses();
+        }
+
+        public override void AddObservationsInput(ref float[] input)
+        {
+            input = raySensor.AnalyzeSensors(); //input of 0 means far from collider, input of 1 means close to collider
+        }
+
+        public override void AddPlayerInput(ref float[] input)
+        {
+            input[0] = Input.GetAxis("Horizontal");
+            input[1] = Input.GetAxis("Vertical");
+            //input[2] = Input.GetAxis("Jump");
+        }
+
+        public override void OnReset()
+        {
+            transform.position = startPosition;
+            transform.rotation = startRotation;
+
+            wheelColliders[(int)WheelPosition.FrontLeft].motorTorque = 0;
+            wheelColliders[(int)WheelPosition.FrontRight].motorTorque = 0;
+
+            wheelColliders[(int)WheelPosition.FrontLeft].brakeTorque = 0;
+            wheelColliders[(int)WheelPosition.FrontRight].brakeTorque = 0;
+
+            wheelColliders[(int)WheelPosition.FrontLeft].steerAngle = 0;
+            wheelColliders[(int)WheelPosition.FrontRight].steerAngle = 0;
+
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            UpdateWheelPoses();
+        }
+        #endregion
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+
+            if (playerControlled)
+                Camera.main.gameObject.SetActive(false);
+
+            startPosition = transform.position;
+            startRotation = transform.rotation;
+
+            wheelColliders = new List<WheelCollider>();
+            for (int i = 0; i < wheelCollidersParentTransform.childCount; i++)
+            {
+                wheelColliders.Add(wheelCollidersParentTransform.GetChild(i).GetComponent<WheelCollider>());
+            }
+        }
+
+        private void Accelerate()
+        {
+            wheelColliders[(int)WheelPosition.FrontLeft].motorTorque = verticalInput * carSpeed;
+            wheelColliders[(int)WheelPosition.FrontRight].motorTorque = verticalInput * carSpeed;
+
+            wheelColliders[(int)WheelPosition.FrontLeft].brakeTorque = brakeInput * brakeSpeed;
+            wheelColliders[(int)WheelPosition.FrontRight].brakeTorque = brakeInput * brakeSpeed;
+        }
+
+        private void Steer()
+        {
+            steeringAngle = maxSteerAngle * horizontalInput;
+
+            wheelColliders[(int)WheelPosition.FrontLeft].steerAngle = steeringAngle;
+            wheelColliders[(int)WheelPosition.FrontRight].steerAngle = steeringAngle;
+        }
+
+        private void UpdateWheelPoses()
+        {
+            for (int i = 0; i < wheelTransforms.Count; i++)
+            {
+                Vector3 pos = wheelTransforms[i].position;
+                Quaternion quat = wheelTransforms[i].rotation;
+
+                wheelColliders[i].GetWorldPose(out pos, out quat);
+                wheelTransforms[i].position = pos;
+                wheelTransforms[i].rotation = quat;
+            }
+        }
+    }
+}
+
